d3d5e4a883541faff1fbeb062533924abe2e6554
swatantra <swatantra0723@gmail.com>
2021-12-05 09:59:23 +0545

    link

--- components/navbar.js
+++ components/navbar.js
@@ -25,15 +25,15 @@ export default function Navbar() {
                     id="nav-content">
                     <ul className="list-reset lg:flex justify-end flex-1 items-center">
                         <li className="mr-3">
-                            <Link className="inline-block py-2 px-4 text-black font-bold no-underline" href="#">Active</Link>
+                            <Link className="inline-block py-2 px-4 text-black font-bold no-underline" href="/">Home</Link>
                         </li>
                         <li className="mr-3">
                             <Link className="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
-                               href="#">link</Link>
+                               href="/profile">Profile</Link>
                         </li>
                         <li className="mr-3">
                             <Link className="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
-                               href="#">link</Link>
+                               href="/contact">Contact</Link>
                         </li>
                     </ul>
                     <button
