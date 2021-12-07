fbf3cae49a2ad5e615f42df74cf4efc24e87af70
swatantra <swatantra0723@gmail.com>
2021-12-07 16:05:35 +0545

    progress bar and hero component

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
-import 'tailwindcss/tailwind.css'
+import 'tailwindcss/tailwind.css';
 import Layout from "../components/layout";
+import NextNProgress from 'nextjs-progressbar';
 
-function MyApp({Component, pageProps}) {
-    return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
-    )
+function MyApp ( { Component, pageProps } ) {
+	return (
+		<Layout>
+			<NextNProgress color="#B621FE" />
+			<Component { ...pageProps } />
+		</Layout>
+	);
 }
-export default MyApp
+export default MyApp;
